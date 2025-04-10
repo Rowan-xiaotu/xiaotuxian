441ebc29411083a31911b44291cf412989ae2a98
Rowan-xiaotu <3583128442@qq.com>
2025-04-10 22:20:22 +0800

    home-hot图片懒加载实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -27,3 +27,10 @@ export const findNewAPI = () => {
         url:'/home/new'
     })
 }
+
+
+export const getHotAPI = () => {
+    return httpInstance({
+        url:'/home/hot'
+    })
+}
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import { useIntersectionObserver } from '@vueuse/core'
 
 
 const app = createApp(App)
@@ -17,3 +17,24 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+
+// 定义全局指令
+
+app.directive('img-lazy',{
+    mounted(el,binding) {
+        // el: 指令绑定的那个元素  img
+        // binding: binding.value 指令等于号后面绑定的表达式的值  图片url
+        console.log(el,binding.value);
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }], ) => {
+              console.log(isIntersecting);
+                if (isIntersecting) {
+                    // 进入视口地址
+                    el.src = binding.value
+                }
+            },
+          )
+    }
+})
