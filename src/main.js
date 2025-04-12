56a6594f00121972c26401cd233fcffc07c26544
Rowan-xiaotu <3583128442@qq.com>
2025-04-12 10:01:49 +0800

    详情页-全局组件插件化

--- src/main.js
+++ src/main.js
@@ -13,10 +13,13 @@ const app = createApp(App)
 // 引入懒加载指令插件并且注册
 import { lazyPlugin } from './directives'
 
+// 引入全局组件插件
+import { componentPlugin } from './components'
+
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
-
+app.use(componentPlugin)
 
 app.mount('#app')
 
