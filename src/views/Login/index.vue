f48f5e2640a1b474840a2cd62576124e2b937e02
Rowan-xiaotu <3583128442@qq.com>
2025-04-12 12:38:45 +0800

    登录-pinia管理用户数据

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,11 +3,13 @@
 // 表单校验 （账号名+密码）
 
 import { ref } from 'vue'
-import { loginAPI } from "@/apis/user";
 
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useRouter } from 'vue-router';
+
+import { useUserStore } from '@/stores/user';
+const userStore = useUserStore()
 // 1.准备表单对象
 const form = ref({
     account: '',
@@ -52,8 +54,7 @@ const doLogin = () => {
         // 以valid作为判断条件  如果通过校验才执行登录逻辑
         if (valid) {
             // ToDo Login
-            const res = await loginAPI({ account,password })
-            console.log(res);
+            await userStore.getUserInfo({ account,password })
             // 1. 提示用户
             ElMessage({ type: 'success', message: '登陆成功' })
             // 2. 跳转首页
