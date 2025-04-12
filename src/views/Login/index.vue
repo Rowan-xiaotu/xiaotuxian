921f9dee410cb5826f97ff77449b207850c08f33
Rowan-xiaotu <3583128442@qq.com>
2025-04-12 12:05:53 +0800

    登陆基础业务实现

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,7 +3,11 @@
 // 表单校验 （账号名+密码）
 
 import { ref } from 'vue'
+import { loginAPI } from "@/apis/user";
 
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
+import { useRouter } from 'vue-router';
 // 1.准备表单对象
 const form = ref({
     account: '',
@@ -38,17 +42,29 @@ const rules = {
 }
 // 3. 获取form实例做同意校验
 const formRef = ref(null)
+const router = useRouter()
 const doLogin = () => {
+    const { account,password } = form.value
     // 调用实例方法
-    formRef.value.validate((valid) => {
+    formRef.value.validate(async (valid) => {
         // valid: 所有表单都通过校验  才为true
         console.log(valid);
         // 以valid作为判断条件  如果通过校验才执行登录逻辑
         if (valid) {
             // ToDo Login
+            const res = await loginAPI({ account,password })
+            console.log(res);
+            // 1. 提示用户
+            ElMessage({ type: 'success', message: '登陆成功' })
+            // 2. 跳转首页
+            router.replace({ path: '/' })
         }
     })
 }
+
+// 1. 用户名和密码  只需要通过简单的配置 （看文档的方式 - 复杂功能通过多个不同组件拆解）
+// 2. 同意协议  自定义规则 validator:(rule,value,callback) => {}
+// 3. 统一校验  通过调用from实例的方法 validate -> true
 </script>
 
 
