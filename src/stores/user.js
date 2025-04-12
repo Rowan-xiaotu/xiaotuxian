aeeaf0cd7f36265275726f5e1e73910666e29f29
Rowan-xiaotu <3583128442@qq.com>
2025-04-12 16:22:55 +0800

    登录-退出登陆业务实现

--- src/stores/user.js
+++ src/stores/user.js
@@ -11,11 +11,16 @@ export const useUserStore = defineStore('user', () =>{
         const res = await loginAPI({ account,password })
         userInfo.value = res.result
     }
+    const clearUserInfo = () => {
+        userInfo.value = {}
+    }
+
 
     // 3. 以对像的格式把state和action return
     return {
         userInfo,
-        getUserInfo
+        getUserInfo,
+        clearUserInfo
     }
 },{
     persist: true,
