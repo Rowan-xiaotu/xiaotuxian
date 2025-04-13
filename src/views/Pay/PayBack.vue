2e3ba6938c03971b537083914397fd587c3120f3
Rowan-xiaotu <3583128442@qq.com>
2025-04-14 00:06:58 +0800

    倒计时逻辑函数实现

--- src/views/Pay/PayBack.vue
+++ src/views/Pay/PayBack.vue
@@ -1,14 +1,13 @@
 <script setup>
-import { getOrderAPI } from '@/apis/pay';
-import {onMounted,ref} from 'vue'
-import { useRoute } from 'vue-router';
-
-
+import { getOrderAPI } from '@/apis/pay'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
 const route = useRoute()
 const orderInfo = ref({})
+
 const getOrderInfo = async () => {
-    const res = await getOrderAPI(route.query.orderId)
-    orderInfo.value = res.result
+  const res = await getOrderAPI(route.query.orderId)
+  orderInfo.value = res.result
 }
 
 onMounted(() => getOrderInfo())
@@ -25,10 +24,10 @@ onMounted(() => getOrderInfo())
         <!-- 路由参数获取到的是字符串而不是布尔值 -->
         <span class="iconfont icon-queren2 green" v-if="$route.query.payResult === 'true'"></span>
         <span class="iconfont icon-shanchu red" v-else></span>
-        <p class="tit">支付{{ $route.query.payResult === 'true'?'成功' : '失败'}}</p>
+        <p class="tit">支付{{ $route.query.payResult === 'true'? '成功' : '失败' }}</p>
         <p class="tip">我们将尽快为您发货，收货期间请保持手机畅通</p>
         <p>支付方式：<span>支付宝</span></p>
-        <p>支付金额：<span>¥{{orderInfo.payMoney.toFixed(2)}}</span></p>
+        <p>支付金额：<span>¥{{orderInfo.payMoney?.toFixed(2)}}</span></p>
         <div class="btn">
           <el-button type="primary" style="margin-right:20px">查看订单</el-button>
           <el-button>进入首页</el-button>
