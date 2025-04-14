8c3b0ecc642ebef2f5adf8d684646c24ad1f7f87
Rowan-xiaotu <3583128442@qq.com>
2025-04-14 19:34:06 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -44,13 +44,4 @@ export default defineConfig({
       }
     }
   },
-  server: {
-    proxy: {
-      '/api': {  // 将所有以 /api 开头的请求代理到目标服务器
-        target: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
-        changeOrigin: true,  // 必须设置为 true 以允许跨域请求
-        rewrite: (path) => path.replace(/^\/api/, '') // 可选：重写路径，移除/api 前缀
-      }
-    }
-  }
 })
