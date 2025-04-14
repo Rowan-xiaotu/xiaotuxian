751d9cfd3b349f6a7e5ddc897b45ab4b33abd202
Rowan-xiaotu <3583128442@qq.com>
2025-04-14 11:03:22 +0800

    订单-分页逻辑实现

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -13,7 +13,7 @@ const tabTypes = [
 ]
 // 获取订单列表
 const orderList = ref([])
-
+const total = ref(0)
 const params = ref({
     orderState: 0,
     page: 1,
@@ -23,6 +23,7 @@ const params = ref({
 const getOrderList = async () => {
     const res = await getUserOrder(params.value)
     orderList.value = res.result.items
+    total.value = res.result.counts
 }
 
 
@@ -35,7 +36,13 @@ const tabChange = (type) => {
     params.value.orderState = type
     getOrderList()
 }
+// 页数切换
+const pageChange = (page) => {
+    console.log(page);
+    params.value.page = page
+    getOrderList()
 
+}
 </script>
 
 <template>
@@ -118,7 +125,7 @@ const tabChange = (type) => {
           </div>
           <!-- 分页 -->
           <div class="pagination-container">
-            <el-pagination background layout="prev, pager, next" />
+            <el-pagination :total="total" @current-change="pageChange" :page-size="params.pageSize" background layout="prev, pager, next" />
           </div>
         </div>
       </div>
